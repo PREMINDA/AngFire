753487d12177f5d22190edad642acf071eb2ce80
PREMINDA <premindabandara0777@gmail.com>
2022-04-11 01:42:20 +0530

    Add Snack bar for autError

--- src/app/service/snack.service.ts
+++ src/app/service/snack.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackService {
 
-  constructor() { }
+  constructor(
+    private snackBar:MatSnackBar,
+    private router:Router
+    ) { }
+
+    aurhError(){
+      this.snackBar.open('You must logged in!','OK',{
+        duration:5000
+      });
+
+      return this.snackBar._openedSnackBarRef
+        ?.onAction()
+        .pipe(
+          tap(_=>this.router.navigate(['/login']))
+        ).subscribe();
+    }
 }
